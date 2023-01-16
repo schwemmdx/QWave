4fc04c5c38c84ed34f868aece48dd774d022d68d
schwemmdx <sven.prigann@web.de>
2023-01-16 21:12:58 +0100

    further fuckarounds with cursors

---
+++ xcursor.h
@@ -0,0 +1,38 @@
+#ifndef XCURSOR_H
+#define XCURSOR_H
+
+#include <QObject>
+#include <QGraphicsItem>
+#include <QWidget>
+#include <QChart>
+#include "theme_colors.h"
+
+class XCursor : public QGraphicsWidget
+{
+    Q_OBJECT
+public:
+    explicit XCursor(QChart* chart);
+    double getPos();
+
+public slots:
+    void placeByClick(QPointF,int num);
+    void remove();
+
+private:
+
+    QGraphicsLineItem* m_line;
+    QGraphicsTextItem* m_markerNumber;
+    QGraphicsTextItem* m_upperText;
+    QGraphicsTextItem* m_text;
+
+    QChart* pChart;
+    QLineF* line;
+    QColor markerRed{Altium::HighLight2};
+
+    QPen markerPen{markerRed,2};
+    QVector<QGraphicsTextItem*> yValues;
+    double mappedPosition;
+
+};
+
+#endif // XCURSOR_H
