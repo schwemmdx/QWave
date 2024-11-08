581816e9238e44c59614d290e6f45fb3122cc0b6
schwemmdx <sven.prigann@web.de>
2024-11-08 13:44:06 +0100

    removed transloations forced fusion style off application

--- main.cpp
+++ main.cpp
@@ -9,19 +9,12 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    QTranslator translator;
-    const QStringList uiLanguages = QLocale::system().uiLanguages();
-    for (const QString &locale : uiLanguages) {
-        const QString baseName = "QWave_" + QLocale(locale).name();
-        if (translator.load(":/i18n/" + baseName)) {
-            a.installTranslator(&translator);
-            break;
-        }
-    }
-
-
-
-
+    a.setStyle("Fusion");
+    
+    //QFile stylesheetFile("./themes/altium.qss");
+    //stylesheetFile.open(QFile::ReadOnly);
+    //QString stylesheet = QLatin1String(stylesheetFile.readAll());
+    //a.setStyleSheet(stylesheet);
     MainWindow w;
     w.set_pMain(&a);
     //w.setWindowState(Qt::WindowMaximized);
