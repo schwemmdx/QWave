a78e4573a14d9496eef8f8b501b5850aed664492
schwemmdx <sven.prigann@web.de>
2024-11-18 08:15:50 +0100

    added multi colum csv file

--- data_gen.py
+++ data_gen.py
@@ -1,11 +1,18 @@
 import numpy as np 
 
 
-x = np.geomspace(1e-3,1e2,num=1000)
+x = np.geomspace(1e-3,1e2,num=int(1e4))
 
-y = np.sin(x*np.pi*2);
+y1 = np.sin(x*np.pi*2)
+y2 = np.cos(x)
+y3 = np.tan(x)
+y4 = np.sin(x)*np.cos(x)
+y5 = x**2
 
-with open("data/gen2.csv","w") as pcsv:
-    pcsv.write("x,y\ns,y\n")
-    for xi,yi in zip(x,y):
-        pcsv.write(f"{xi},{yi}\n")
+with open("data/large_set.csv","w") as pcsv:
+    pcsv.write("time,y1,y2,y3,y4,y5\ns,a,b,c,d,e\n")
+    for xi,a1,a2,a3,a4,a5 in zip(x,y1,y2,y3,y4,y5):
+        pcsv.write(f"{xi},{a1},{a2},{a3},{a4},{a5}\n")
+
+
+        
